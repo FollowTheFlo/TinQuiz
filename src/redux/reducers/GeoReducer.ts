e70e3832d572c7c487b900af45bb40760c0e99c2
FollowTheFlo <florent.letendre@hotmail.fr>
2021-12-14 01:09:47 -0500

    prettify

--- src/redux/reducers/GeoReducer.ts
+++ src/redux/reducers/GeoReducer.ts
@@ -1,13 +1,16 @@
 //import { Location} from './../store';
 import { Action } from "../actions";
-import { EndLocationAction, SetLocationFromFlagAction } from './../actions/geoActions'
-import{
+import {
+  EndLocationAction,
+  SetLocationFromFlagAction,
+} from "./../actions/geoActions";
+import {
   START_LOCATION,
   END_LOCATION,
   SHOW_GEO_ERROR,
   CLEAR_GEO_ERROR,
   SET_LOCATION_FROM_FLAG,
-  } from "./../constants";
+} from "./../constants";
 
 export interface Location {
   id: string;
@@ -19,85 +22,88 @@ export interface Location {
   countryWD: string;
   lat: number;
   lng: number;
-};
-
-export interface GeoState {
-
-    location:Location,
-    errorMessage: string,
-    loading: boolean,
 }
 
-const initialState = {  
-    errorMessage: '',
-    location: {id:'0',
-    place:'Saint-James',
-    region:'Manche',
-    country:'France',
-    placeWD:'Q478259',
-    regionWD:'Q12589',
-    countryWD:'Q142',
-    lat:-1.325183,
-    lng:48.523252},
-    loading: false,
-    
+export interface GeoState {
+  location: Location;
+  errorMessage: string;
+  loading: boolean;
+}
+
+const initialState = {
+  errorMessage: "",
+  location: {
+    id: "0",
+    place: "Montreal",
+    region: "Manche",
+    country: "Canada",
+    placeWD: "Q478259",
+    regionWD: "Q12589",
+    countryWD: "Q16",
+    lat: -1.325183,
+    lng: 48.523252,
+  },
+  loading: false,
 };
 
-export const locationReducer =  (state:GeoState = initialState, action: Action): GeoState => {
-
-
+export const locationReducer = (
+  state: GeoState = initialState,
+  action: Action
+): GeoState => {
   switch (action.type) {
-   
-      case START_LOCATION: {
-        console.log('REDUCER - START_LOCATION: ');
-        return {  
-          ...state, loading: true
-        };
-      }
-      case END_LOCATION: {
-        console.log('REDUCER - END_LOCATION: ');
-        const endLocationAction = action as EndLocationAction;
-        return {           
-          ...state,
-          loading: false,
-          //target: endLocationAction.payload.location.place,
-          location: endLocationAction.payload.location
-        };
-      }
-      case SHOW_GEO_ERROR: {
-        console.log('REDUCER - SHOW_ERROR: ', action.payload);
-        return {
-            // @ts-ignore
-          ...state, errorMessage: action.payload, loading:false
-        };
-      }
-      case CLEAR_GEO_ERROR: {
-        console.log('REDUCER - CLEAR_ERROR ');
-        return {         
-          ...state, errorMessage: ""
-        };
-      }
-      case SET_LOCATION_FROM_FLAG: {
-        const setLocationFromFlagAction = action as SetLocationFromFlagAction;
-        console.log('REDUCER - SET_LOCATION_FROM_FLAG ');
-        return {         
-          ...state, location: {
-            id: setLocationFromFlagAction.payload.flag.WdCode,
-            place: '',
-            region: '',
-            country: setLocationFromFlagAction.payload.flag.label,
-            placeWD: '',
-            regionWD: '',
-            countryWD: setLocationFromFlagAction.payload.flag.WdCode,
-            lat: 0,
-            lng: 0,
-          }
-        };
-      }
-
-    
+    case START_LOCATION: {
+      console.log("REDUCER - START_LOCATION: ");
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+    case END_LOCATION: {
+      console.log("REDUCER - END_LOCATION: ");
+      const endLocationAction = action as EndLocationAction;
+      return {
+        ...state,
+        loading: false,
+        //target: endLocationAction.payload.location.place,
+        location: endLocationAction.payload.location,
+      };
+    }
+    case SHOW_GEO_ERROR: {
+      console.log("REDUCER - SHOW_ERROR: ", action.payload);
+      return {
+        ...state,
+        // @ts-ignore
+        errorMessage: action.payload,
+        loading: false,
+      };
+    }
+    case CLEAR_GEO_ERROR: {
+      console.log("REDUCER - CLEAR_ERROR ");
+      return {
+        ...state,
+        errorMessage: "",
+      };
+    }
+    case SET_LOCATION_FROM_FLAG: {
+      const setLocationFromFlagAction = action as SetLocationFromFlagAction;
+      console.log("REDUCER - SET_LOCATION_FROM_FLAG ");
+      return {
+        ...state,
+        location: {
+          id: setLocationFromFlagAction.payload.flag.WdCode,
+          place: "",
+          region: "",
+          country: setLocationFromFlagAction.payload.flag.label,
+          placeWD: "",
+          regionWD: "",
+          countryWD: setLocationFromFlagAction.payload.flag.WdCode,
+          lat: 0,
+          lng: 0,
+        },
+      };
+    }
 
     default:
       return state;
   }
-}
+};
